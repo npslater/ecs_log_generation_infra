49332d601d5210d6d12ca310eca11a159faf2070
Nathaniel Slater <nslater@Nathaniels-MacBook-Pro.local>
2022-11-02 11:41:26 -0700

    Initial commit

---
+++ test/ecs_log_generation.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EcsLogGeneration from '../lib/ecs_log_generation-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ecs_log_generation-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EcsLogGeneration.EcsLogGenerationStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
